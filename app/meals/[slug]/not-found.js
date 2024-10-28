0f5ccb5fbbf12cbda7028a1033b2f1842e3466ec
kamel <kamelsamy3@gmail.com>
2024-10-28 21:10:39 +0300

    adding page meal to show the details of the meal you clicked on it and error if searched about meal not found

---
+++ app/meals/[slug]/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFoundMeal(){
+    return(
+        <>
+            <h1>Error Occurred</h1>
+            <p>The Meal is Not Found</p>
+        </>
+        
+    )
+}
--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,32 @@
-export default function MealDetails({props}){
+import Image from "next/image";
+import { getMeal } from "@/lib/meal";
+import { notFound } from "next/navigation";
+
+export default function MealDetails({params}){
+    const meal =getMeal(params.slug);
+    if(!meal){
+        notFound();
+    }
+    meal.instructions=meal.instructions.replace(/\n/g,'<br>');
     return(
         <>
-        <h1>MealDetails</h1>
-        {/* <p>{props.slug}</p> */}
+            <header>
+                <div>
+                    <Image src={meal.image} alt={meal.title} width={300} height={300}/>
+                </div>
+                <div>
+                    <h1>{meal.title}</h1>
+                    <p>
+                        by <a href={`mailto:${meal['creator_email']}`}>meal.creator</a>
+                    </p>
+                    <p>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <div dangerouslySetInnerHTML={{
+                    __html:meal.instructions
+                }}></div>
+            </main>
         </>
         
 
